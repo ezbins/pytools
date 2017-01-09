8b13bad7207c8761ae1993b3e48bb353f87d0277
Dennis Peng <ty3627@gmail.com>
2017-01-09 18:02:22 +0800

    增加try cache

--- java_pid_check.py
+++ java_pid_check.py
@@ -4,22 +4,26 @@ from pexpect import pxssh
 import datetime as dt
 
 def ssh_login(ip,user,pw):
-    ssh = pxssh.pxssh()
-    ssh.login(ip, user, pw)
-    ssh.sendline('pidof java')
-    ssh.prompt()
+    try:
+        ssh = pxssh.pxssh()
+        ssh.login(ip, user, pw)
+        ssh.sendline('pidof java')
+        ssh.prompt()
 
-    with open('pidjava.log', 'a') as f:
-        f.write(str(dt.datetime.now()))
-        f.write("\n")
-        f.write(ip)
-        f.write("\n")
-        f.write(ssh.before.decode('utf-8'))
+        with open('pidjava.log', 'a') as f:
+            f.write(str(dt.datetime.now()))
+            f.write("\n")
+            f.write(ip)
+            f.write("\n")
+            f.write(ssh.before.decode('utf-8'))
 
-    ssh.logout()
-    ssh.close()
+        ssh.logout()
+        ssh.close()
 
-    return True
+        return True
+    except pxssh.ExceptionPxssh:                
+        return False
+    
 
 with open('sshid','r') as fin:
     for line in fin:
@@ -27,4 +31,4 @@ with open('sshid','r') as fin:
         if ssh_login(ip,user,pw):
             print("Success!")
         else:
-            print("something wrong!!")
+            print("check ip,username or password")
--- ssh_login.py
+++ ssh_login.py
@@ -3,15 +3,17 @@
 from pexpect import pxssh
 import getpass
 
-ssh = pxssh.pxssh()
-
-#passwd = getpass.getpass()
-ssh.login('52.74.188.31','root',ssh_key="/home/shao/.ssh/cobain-singapore.pem")
-ssh.sendline('date')
-ssh.prompt()
-print(ssh.before.decode('utf-8'))
-ssh.logout()
-
+try:
+    ssh = pxssh.pxssh()
+    #passwd = getpass.getpass()
+    ssh.login('52.74.188.31','root',ssh_key="/home/shao/.ssh/cobain-singapore.pem")
+    ssh.sendline('date')
+    ssh.prompt()
+    print(ssh.before.decode('utf-8'))
+    ssh.logout()
+    ssh.close()
+except pxssh.ExceptionPxssh:
+    print("check ip,username or password")
 
 
 
